a86cd6705b316108e2110a8a7ebdd1cb8b5b049d
RozziniHenrique <hrozzini@gmail.com>
2025-09-03 00:09:04 -0300

    Programas JAVA

---
+++ Codigos/javaCarro/uscsTestaCarro.java
@@ -0,0 +1,20 @@
+package Java.Codigos.javaCarro;
+
+public class uscsTestaCarro {
+    public static void main(String[] args) {
+        // Criação dos objetos Carro
+        Carro corolla = new Carro("Toyota", "Corolla", 2020);
+        Carro nivus = new Carro("Volkswagen", "Nivus", 2021);
+        Carro civic = new Carro("Honda", "Civic", 2019);
+
+        // Operações com os carros
+        corolla.ligar();
+        corolla.acelerar();
+
+        nivus.ligar();
+        nivus.acelerar();
+
+        civic.ligar();
+        civic.acelerar();
+    }
+}
